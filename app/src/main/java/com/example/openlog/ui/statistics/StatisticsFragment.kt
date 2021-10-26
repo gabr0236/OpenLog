b2e975ceee30e7e0e06ca6cea3a68696f4889162
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-10-26 15:56:05 +0200

    StatisticsFragment.kt mostly done, missing recyclerview at the bottom for selecting log to edit. fragment_edit_log.xml added. Fixed compiling issues.

--- app/src/main/java/com/example/openlog/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/openlog/ui/statistics/StatisticsFragment.kt
@@ -29,11 +29,6 @@ class StatisticsFragment : Fragment() {
 
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textStatistics
-        statisticsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
