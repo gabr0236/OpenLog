58c0a94d7b216e930bf8003a8d1f3aa41f7576b6
Gabriel Haugbøl <gabriel.haugbol@gmail.com>
2022-01-03 23:14:23 +0100

    more emojis added to spinner

--- app/src/main/java/com/example/openlog/util/EmojiRetriever.kt
+++ app/src/main/java/com/example/openlog/util/EmojiRetriever.kt
@@ -11,6 +11,28 @@ class EmojiRetriever {
                 2 -> emojiArray[1]
                 3 -> emojiArray[2]
                 4 -> emojiArray[3]
+                5 -> emojiArray[4]
+                6 -> emojiArray[5]
+                7 -> emojiArray[6]
+                8 -> emojiArray[7]
+                9 -> emojiArray[8]
+                10 -> emojiArray[9]
+                11 -> emojiArray[10]
+                12 -> emojiArray[11]
+                13 -> emojiArray[12]
+                14 -> emojiArray[13]
+                15 -> emojiArray[14]
+                16 -> emojiArray[15]
+                17 -> emojiArray[16]
+                18 -> emojiArray[17]
+                19 -> emojiArray[18]
+                20 -> emojiArray[19]
+                21 -> emojiArray[20]
+                22 -> emojiArray[21]
+                23 -> emojiArray[22]
+                24 -> emojiArray[23]
+                25 -> emojiArray[24]
+                26 -> emojiArray[25]
                 else -> R.drawable.ic_baseline_image_not_supported_24
             }
         }
@@ -18,8 +40,32 @@ class EmojiRetriever {
         val emojiArray: Array<Int> = arrayOf(
             R.drawable.emoji_calories,
             R.drawable.emoji_sugar_blood_level,
+            R.drawable.emoji_insulin,
             R.drawable.emoji_sleep,
-            R.drawable.emoji_insulin
+            R.drawable.emoji_arm,
+            R.drawable.emoji_books,
+            R.drawable.emoji_burger,
+            R.drawable.emoji_calculator,
+            R.drawable.emoji_cashless_payment,
+            R.drawable.emoji_creative_thinking,
+            R.drawable.emoji_dart_board,
+            R.drawable.emoji_diagram,
+            R.drawable.emoji_graphic_design,
+            R.drawable.emoji_hammer,
+            R.drawable.emoji_moon,
+            R.drawable.emoji_mop,
+            R.drawable.emoji_mortarboard,
+            R.drawable.emoji_music_player,
+            R.drawable.emoji_namaste,
+            R.drawable.emoji_notebook,
+            R.drawable.emoji_open_book,
+            R.drawable.emoji_painting,
+            R.drawable.emoji_seeding,
+            R.drawable.emoji_sword,
+            R.drawable.emoji_uterus,
+            R.drawable.emoji_videogame,
+            R.drawable.emoji_yoga_pose2,
+            R.drawable.emoji_yoga_pose3,
         )
     }
 }
